20564488511e199ce7219f0c80a8de48d13ed3f8
Jesse <jesse.david@live.ca>
2017-11-21 17:04:47 -0500

    added red x

--- iat.js
+++ iat.js
@@ -1,13 +1,13 @@
 const hello = document.getElementById('hello');
 const gender = document.getElementById('gender');
 
-const bias = ["blackwhitedemo/1.gif", "blackwhitedemo/2.gif", "blackwhitedemo/3.gif", "blackwhitedemo/4.gif", "blackwhitedemo/5.gif", "blackwhitedemo/6.gif", "blackwhitedemo/7.gif", "blackwhitedemo/8.gif","blackwhitedemo/9.gif"];
+const bias = ['redx.png', "blackwhitedemo/1.gif", "blackwhitedemo/2.gif", "blackwhitedemo/3.gif", "blackwhitedemo/4.gif", "blackwhitedemo/5.gif", "blackwhitedemo/6.gif", "blackwhitedemo/7.gif", "blackwhitedemo/8.gif","blackwhitedemo/9.gif"];
 
 const goodWords = ['enjoy', 'magnificent', 'triumph', 'cheer', 'pleasure', 'cherish', 'delightful', 'fantastic'];
 
 const badWords = ['tragic', 'awful', 'rotten', 'selfish', 'horrific', 'humiliate', 'grief', 'scorn'];
 
-const tryAgain = ['try again', 'wrong category', 'please select the right category'];
+const tryAgain = ['redx.png'];
 
 
 let i = 0;
@@ -25,17 +25,7 @@ const changeImage = () => {
 
 window.onkeyup = (event) => {
     let key = event.keyCode ? event.keyCode : event.which;
-    // if (key === 69) {
-    //     document.getElementById('gender').innerText = 'girl';
-    //     console.log('left');
-        
-    //     faces.setAttribute("src", bias[i]);
-    //     i++;
-    //     if (bias.length === i) {
-    //         i = 0;
-    //     }
-    // }
-    
+
     // for(let word of goodWords) {
     //     if(key == 69) {
     //         console.log(word);
@@ -45,21 +35,16 @@ window.onkeyup = (event) => {
     // }
 
     if (key == 73) {
-        // document.getElementById('gender').innerText = 'boy';
-        console.log('right');
         hello.innerText = goodWords[i]
         i++;
+        console.log('length', goodWords.length);
         if (goodWords.length === i) {
-            i = 0;
-            
+            console.log('i', i);
+            i = 0;   
         } 
     } else {
         console.log('huh?');
-        gender.innerText = tryAgain[i];
-        i++;
-        if (tryAgain.length === i) {
-            i = 0;
-        }
+        gender.setAttribute('src', tryAgain[0]);
     }
  }
     
